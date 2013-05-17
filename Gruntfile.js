fd05ad8c6495fc2af8e3638c88ae09eac29ef792
hbc <bcxxxxxx@gmail.com>
2013-05-17 22:51:55 +0800

    added grunt support

---
+++ Gruntfile.js
@@ -0,0 +1,44 @@
+module.exports = function(grunt) {
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+        requirejs: {
+            compile: {
+                options: {
+                    baseUrl: 'app',
+                    shim: {
+                        underscore: {
+                            exports: '_'
+                        },
+                        backbone: {
+                            deps: ['underscore', 'jquery'],
+                            exports: 'Backbone'
+                        }
+                    },
+                    paths: {
+                        jquery: 'libs/jquery/jquery.min',
+                        underscore: 'libs/underscore/underscore-min',
+                        backbone: 'libs/backbone/backbone-min',
+                        overflow: 'libs/jquery-overflow/jquery.overflow'
+                    },
+                    name: 'main',
+                    out: 'static/js/app.js'
+                }
+            }
+        },
+        watch: {
+            options: {
+                livereload: true
+            },
+            app: {
+                files: ['app/*'],
+                tasks: ['requirejs:compile']
+            },
+            style: {
+                files: ['static/css/style.css']
+            }
+        }
+    });
+
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-contrib-requirejs');
+};
